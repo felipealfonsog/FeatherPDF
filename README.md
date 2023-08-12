e455b258081fc9e23ff08bd427dd90f14b0f5275
Felipe <f.alfonso@res-ear.ch>
2023-08-12 05:19:18 -0400

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# FeatherPDF
+# FeatherPDF - Lightweight PDF Viewer
+
+![FeatherPDF Logo](logo.png)
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![Python](https://img.shields.io/badge/python-3.11-blue.svg)](https://www.python.org/downloads/release/python-311/)
+[![PyQt5](https://img.shields.io/badge/PyQt5-5.15.9-blue.svg)](https://pypi.org/project/PyQt5/)
+
+## Description
+
+FeatherPDF is an ultra-lightweight PDF viewer designed for Linux and macOS. It provides a simple and intuitive user interface for reading PDF documents. The project aims to offer a fast and minimalistic PDF reading experience while using minimal system resources.
+
+## Features
+
+- Open and view PDF files with ease.
+- Navigate through pages using on-screen controls or mouse scroll.
+- Zoom in and out for comfortable reading.
+- Return to the first page or navigate to the next/previous pages.
+- Responsive and intuitive user interface.
+- MIT Licensed - Free and open-source.
+
+## Version 1.0.0
+
+The current version, 1.0.0, introduces the core functionality of FeatherPDF. Users can open PDF files, navigate through pages, zoom in/out, and enjoy a seamless reading experience.
+
+## Future Developments
+
+Future versions of FeatherPDF will focus on enhancing the user experience and compatibility. Planned features include:
+
+- Windows support.
+- Search functionality within PDF documents.
+- Annotation and highlighting capabilities.
+- Night mode for reduced eye strain.
+- Keyboard shortcuts for efficient navigation.
+
+## Contributing
+
+FeatherPDF is an open-source project, and contributions are welcome! You can contribute in various ways:
+
+- Reporting issues and bugs.
+- Suggesting new features and enhancements.
+- Submitting pull requests for code improvements.
+- Providing translations for internationalization.
+- Spreading the word and sharing the project.
+
+## License
+
+FeatherPDF is licensed under the MIT License.
---
+++ src/old/fpdf.0.0.4.py
@@ -0,0 +1,251 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QImage, QPixmap, QWheelEvent, QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QFileDialog, QGraphicsView, QGraphicsScene, QLabel, QMessageBox, QTabWidget, QSizePolicy, QTabBar
+
+import fitz  # PyMuPDF
+
+class PDFViewer(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
+        layout = QVBoxLayout()
+
+        self.pdf_view = QGraphicsView()
+        self.pdf_scene = QGraphicsScene()
+        self.pdf_view.setScene(self.pdf_scene)
+        self.pdf_view.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.pdf_view.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.pdf_view)
+
+        self.setLayout(layout)
+
+        self.doc = None
+        self.current_page = 0
+        self.zoom_factor = 1.0
+
+    def openPDF(self, file_name):
+        if file_name:
+            self.doc = fitz.open(file_name)
+            self.current_page = 0
+            self.showPage()
+
+    def showPage(self):
+        if self.doc is not None and 0 <= self.current_page < self.doc.page_count:
+            page = self.doc[self.current_page]
+            image = page.get_pixmap(matrix=fitz.Matrix(self.zoom_factor, self.zoom_factor), alpha=False)
+            image_data = image.samples
+
+            qimage = QImage(image_data, image.width, image.height, image.stride, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(qimage)
+            self.pdf_scene.setSceneRect(0, 0, image.width, image.height)
+            self.pdf_scene.clear()
+            self.pdf_scene.addPixmap(pixmap)
+
+    def goHome(self):
+        self.zoom_factor = 1.0
+        self.current_page = 0
+        self.showPage()
+
+    def zoomIn(self):
+        self.zoom_factor *= 1.25
+        self.showPage()
+
+    def zoomOut(self):
+        self.zoom_factor *= 0.8
+        self.showPage()
+
+    def resetZoom(self):
+        self.zoom_factor = 1.0
+        self.showPage()
+
+    def prevPage(self):
+        if self.current_page > 0:
+            self.current_page -= 1
+            self.showPage()
+
+    def nextPage(self):
+        if self.doc is not None and self.current_page < self.doc.page_count - 1:
+            self.current_page += 1
+            self.showPage()
+
+    def wheelEvent(self, event: QWheelEvent):
+        num_degrees = event.angleDelta().y() / 8
+        num_steps = num_degrees / 15
+        
+        if num_steps > 0:
+            self.prevPage()
+        else:
+            self.nextPage()
+
+class FeatherPDF(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('FeatherPDF')
+        self.setGeometry(100, 100, 800, 600)
+        
+        # Agrega el ícono a la ventana
+        icon = QIcon("fpdf-iconlogo.png")
+        self.setWindowIcon(icon)
+
+        layout = QVBoxLayout()
+
+        self.tab_widget = QTabWidget()
+        layout.addWidget(self.tab_widget)
+
+        self.createTab()
+
+        button_layout = QHBoxLayout()
+
+        open_button = QPushButton('Open')
+        open_button.clicked.connect(self.openPDF)
+        open_button.setFixedSize(40, 20)
+        button_layout.addWidget(open_button)
+
+        new_button = QPushButton('New Instance')
+        new_button.clicked.connect(self.createNewInstance)
+        new_button.setFixedSize(80, 20)
+        button_layout.addWidget(new_button)
+
+        home_button = QPushButton('Home')
+        home_button.clicked.connect(self.goHome)
+        home_button.setFixedSize(40, 20)
+        button_layout.addWidget(home_button)
+
+        zoom_in_button = QPushButton('+')
+        zoom_in_button.clicked.connect(self.zoomIn)
+        zoom_in_button.setFixedSize(20, 20)
+        button_layout.addWidget(zoom_in_button)
+
+        zoom_out_button = QPushButton('-')
+        zoom_out_button.clicked.connect(self.zoomOut)
+        zoom_out_button.setFixedSize(20, 20)
+        button_layout.addWidget(zoom_out_button)
+
+        reset_zoom_button = QPushButton('Reset Zoom')
+        reset_zoom_button.clicked.connect(self.resetZoom)
+        reset_zoom_button.setFixedSize(80, 20)
+        button_layout.addWidget(reset_zoom_button)
+
+        prev_page_button = QPushButton('<')
+        prev_page_button.clicked.connect(self.prevPage)
+        prev_page_button.setFixedSize(20, 20)
+        button_layout.addWidget(prev_page_button)
+
+        next_page_button = QPushButton('>')
+        next_page_button.clicked.connect(self.nextPage)
+        next_page_button.setFixedSize(20, 20)
+        button_layout.addWidget(next_page_button)
+
+        self.page_label = QLabel()
+        button_layout.addWidget(self.page_label)
+
+        credits_button = QPushButton('Credits')
+        credits_button.clicked.connect(self.showCredits)
+        credits_button.setFixedSize(80, 20)
+        button_layout.addWidget(credits_button)
+
+        quit_button = QPushButton('Quit')
+        quit_button.clicked.connect(self.quitApp)
+        quit_button.setFixedSize(60, 20)
+        button_layout.addWidget(quit_button)
+
+        layout.addLayout(button_layout)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        self.current_tab = None
+
+    def createTab(self, file_name=None):
+        pdf_tab = PDFViewer()
+        if file_name:
+            pdf_tab.openPDF(file_name)
+        self.tab_widget.addTab(pdf_tab, "Untitled")
+        if file_name:
+            self.tab_widget.setTabText(self.tab_widget.indexOf(pdf_tab), file_name)
+        self.tab_widget.tabBar().setTabButton(self.tab_widget.indexOf(pdf_tab), QTabBar.RightSide, None)  # Elimina el botón de cierre predeterminado
+        pdf_tab.tab_index = self.tab_widget.indexOf(pdf_tab)
+        pdf_tab.close_button = QPushButton('x')
+        pdf_tab.close_button.setFixedSize(20, 20)
+        pdf_tab.close_button.clicked.connect(lambda: self.closeTab(pdf_tab))
+        self.tab_widget.tabBar().setTabButton(self.tab_widget.indexOf(pdf_tab), QTabBar.LeftSide, pdf_tab.close_button)
+
+    def openPDF(self):
+        options = QFileDialog()
+        file_name, _ = options.getOpenFileName(self, "Open PDF File", "", "PDF Files (*.pdf);;All Files (*)")
+        if file_name:
+            self.createTab(file_name)
+
+    def createNewInstance(self):
+        new_instance = FeatherPDF()
+        new_instance.show()
+
+    def closeTab(self, tab):
+        self.tab_widget.removeTab(tab.tab_index)
+
+    def goHome(self):
+        if self.current_tab:
+            self.current_tab.goHome()
+
+    def zoomIn(self):
+        if self.current_tab:
+            self.current_tab.zoomIn()
+
+    def zoomOut(self):
+        if self.current_tab:
+            self.current_tab.zoomOut()
+
+    def resetZoom(self):
+        if self.current_tab:
+            self.current_tab.resetZoom()
+
+    def prevPage(self):
+        if self.current_tab:
+            self.current_tab.prevPage()
+
+    def nextPage(self):
+        if self.current_tab:
+            self.current_tab.nextPage()
+
+    def showCredits(self):
+        credits_text = (
+            "Created by Felipe Alfonso Gonzalez\n"
+            "Computer Science Engineer\n"
+            "Chile, 2023\n"
+            "Email: f.alfonso@res-ear.ch\n"
+            "GitHub: github.com/felipealfonsog\n\n"
+            "Licensed under the MIT License\n\n"
+            "MIT License:\n\n"
+            "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and "
+            "associated documentation files (the 'Software'), to deal in the Software without restriction, "
+            "including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,"
+            " and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,"
+            " subject to the following conditions:\n\n"
+            "The above copyright notice and this permission notice shall be included in all copies or substantial "
+            "portions of the Software.\n\n"
+            "THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT"
+            " LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN "
+            "NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,"
+            " WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE "
+            "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+        )
+        QMessageBox.information(self, "Credits", credits_text)
+
+    def quitApp(self):
+        self.close()
+
+    def tabChanged(self, index):
+        self.current_tab = self.tab_widget.widget(index)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')  # Cambia el estilo a 'Fusion'
+    window = FeatherPDF()
+    window.show()
+    sys.exit(app.exec_())
